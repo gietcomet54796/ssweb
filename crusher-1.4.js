5625e40ff4567f09cbb061bcb2847a3a5dccb860
gietcomet54796 <75877178+gietcomet54796@users.noreply.github.com>
2020-12-21 12:47:32 +0800

    Add files via upload

---
+++ crusher-1.4.js
@@ -0,0 +1,11 @@
+function cpdesc(){
+  window.location.assign("/serv/toport?game=ColoredPong");
+}
+
+function ssbdesc(){
+  window.location.assign("/serv/toport?game=StarShipBattle");
+}
+
+function drdesc(){
+  window.location.assign("/serv/toport?game=DodgeRacing");
+}
